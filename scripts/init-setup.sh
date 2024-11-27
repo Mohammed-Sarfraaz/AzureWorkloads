c2cac5ab0a06cd8742039a9a4a16f8dd9b510279
Sarfraaz <sarfraaz@outlook.com>
2024-11-27 16:55:04 +0000

    updated

--- scripts/init-setup.sh
+++ scripts/init-setup.sh
@@ -2,12 +2,6 @@
 
 set -e
 
-export LOCATION=eastus
-export COMMON_RESOURCE_GROUP_NAME=rg-azure-common-services-rg-mss
-#export TF_STATE_STORAGE_ACCOUNT_NAME=stgtfstate1
-export TF_STATE_CONTAINER_NAME=contfstate1
-export KEYVAULT_NAME=kvazuresecrets
-
 # Create the resource group
 if  ( `az group exists --resource-group $COMMON_RESOURCE_GROUP_NAME` == "true" );
 then
